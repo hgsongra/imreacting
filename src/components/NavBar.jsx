f568780dacd9a208cce106765c16843bdd700bdd
hgsongra <hgsongra@ymail.com>
2019-07-31 00:44:31 +0530

    Convert NavBar componenent to Stateless componenet

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class NavBar extends Component {
-  render() {
-    return (
-      <nav className='navbar navbar-light bg-light'>
-        Navbar{" "}
-        <span className='badge badge-secondary badge-pill'>
-          {this.props.total}
-        </span>
-      </nav>
-    );
-  }
-}
+const NavBar = props => {
+  return (
+    <nav className='navbar navbar-light bg-light'>
+      Navbar{" "}
+      <span className='badge badge-secondary badge-pill'>{props.total}</span>
+    </nav>
+  );
+};
+
+export default NavBar;
